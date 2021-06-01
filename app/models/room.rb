3908bc6a8d7b41781740f343bd6d2fe81b74e617
Fabien <cacrfabfr@gmail.com>
2021-06-01 11:55:58 +0200

    added the Room model, check the has_many"

---
+++ app/models/room.rb
@@ -0,0 +1,12 @@
+class Room < ApplicationRecord
+  has_many :messages
+  has_many :users, through: :messages
+  has_many :room_interests
+  has_many :interests, through: :room_interests
+  has_many :surveys
+  has_many :room_topics
+  has_many :topics, through: :room_topics
+
+  validates :name, presence: true
+  validates :description, presence: true
+end
