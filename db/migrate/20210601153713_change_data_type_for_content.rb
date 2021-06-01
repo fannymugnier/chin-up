ec18ab06db6e62765f3d6cba7a19dae59223914e
Fabien <cacrfabfr@gmail.com>
2021-06-01 17:39:20 +0200

    change column type

---
+++ db/migrate/20210601153713_change_data_type_for_content.rb
@@ -0,0 +1,5 @@
+class ChangeDataTypeForContent < ActiveRecord::Migration[6.0]
+  def change
+    change_column :messages, :content, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_145602) do
+ActiveRecord::Schema.define(version: 2021_06_01_153713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_145602) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.text "content"
+    t.string "content"
     t.bigint "user_id", null: false
     t.bigint "room_id", null: false
     t.datetime "created_at", precision: 6, null: false
