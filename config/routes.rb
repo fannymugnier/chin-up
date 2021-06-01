160986781636eb5ebe22a11182e5e555e4514954
Fabien <cacrfabfr@gmail.com>
2021-06-01 14:55:13 +0200

    created the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :rooms, only [:index, :show] do
+    resources :messages, only [:create]
+    resources :surveys, only [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
