66bdbf64efda6bef3d2f4e86867e4abad042a6f4
Fabien <cacrfabfr@gmail.com>
2021-06-03 16:44:31 +0200

    creted the 'sondage' section

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,13 +2,15 @@ class SurveysController < ApplicationController
   def create
     @survey = Survey.new(survey_params)
     @room = Room.find(params[:room_id])
+    @survey.room = @room
+    @survey.author = current_user
     if @survey.save
-      @message = Message.create(content: 'a lancé un nouveau sondage.', message_type: 'announce', user: current_user)
+      @message = Message.create(room: @room, content: 'a lancé un nouveau sondage.', message_type: 'announce', user: current_user)
       RoomChannel.broadcast_to(
         @room,
-        render_to_string(partial: "announce", locals: { message: @message })
+        render_to_string(partial: "messages/announce", locals: { message: @message })
       )
-      redirect_to room_path(@room)
+      redirect_to room_path(@room, anchor: "message-#{@message.id}")
     else
       render "rooms/show"
     end
@@ -17,6 +19,6 @@ class SurveysController < ApplicationController
   private
 
   def survey_params
-    params.require(:survey).permit(:title, :first_proposition, :second_proposition)
+    params.require(:survey).permit(:title, :first_proposition, :second_proposition, :first_photo, :second_photo)
   end
 end
