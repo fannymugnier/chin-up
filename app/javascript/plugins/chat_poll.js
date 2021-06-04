4f1d5d550961a25fd44f3cb4e7e76a87946f24fb
Fabien <cacrfabfr@gmail.com>
2021-06-04 16:23:20 +0200

    survey result style

---
+++ app/javascript/plugins/chat_poll.js
@@ -0,0 +1,7 @@
+// first proposition
+const firstProposition = document.querySelector(".proposition.first");
+// second proposition
+const secondProposition = document.querySelector(".proposition.second");
+// poll div
+const pollDiv = document.querySelector(".proposition");
+// TODO ...
