d6e614e80606709b461db172df74d007e1408250
Fabien <cacrfabfr@gmail.com>
2021-06-07 17:20:16 +0200

    Added modal when clicking on User, need to add the functionality

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,18 +2,18 @@ class RoomsController < ApplicationController
   before_action :set_room, only: :show
   before_action :set_entities, only: :show
 
- def index
-  @rooms = Room.all
-  if params[:query].present?
-  sql = " \
+  def index
+    @rooms = Room.all
+    if params[:query].present?
+      sql = " \
     rooms.name @@ :query \
     OR interests.name @@ :query \
     "
-    @rooms = Room.joins(:interests).where(sql, query: "%#{params[:query]}%").distinct
-  else
-    @rooms = Room.all
+      @rooms = Room.joins(:interests).where(sql, query: "%#{params[:query]}%").distinct
+    else
+      @rooms = Room.all
+    end
   end
-end
 
   def show
     @ongoing_survey = @room.surveys.find_by(status: "open")
