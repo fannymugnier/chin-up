a52dba84561f803023d1b3d9b0a0c68d098a8cb8
Gilles Roques <gilles.roques@gmail.com>
2021-06-08 16:59:33 +0200

    suggestion-topic-interest

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if current_user
+      @user_interests = current_user.interests
+      @match_interest_rooms = Room.joins(:room_interests).where(room_interests: { interest: @user_interests })
+      @unmatch_rooms = Room.where.not(id: @match_interest_rooms.pluck(:id))
+      @suggested_rooms = (@match_interest_rooms + @unmatch_rooms).first(3)
+    else
+      @suggested_rooms = Room.all.sample(3)
+    end
+
     @rooms = Room.all
   end
 end
