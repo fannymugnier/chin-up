6a02df85f6a5f40b86ce1c14bcf6bcdadd5a7f0d
Fanny Mugnier <fanny.mugnier@outlook.com>
2021-06-10 10:58:22 +0200

    redesign topic&survey buttons

--- app/javascript/plugins/expand-btn.js
+++ app/javascript/plugins/expand-btn.js
@@ -2,12 +2,14 @@ const expandBtn = () => {
 
   const expand = document.querySelectorAll('.expand-button')
   if (expand) {
+    const previousBtnElement = document.querySelector('#new-survey-btn')
     expand.forEach(button => {
       button.addEventListener('click', () => {
         const expandContent = document.querySelector('.expand-content');
-        
+
         button.classList.toggle('expand-button--active')
-        
+        previousBtnElement.classList.toggle('remove-bdrs')
+
         if (button.classList.contains('expand-button--active')) {
           expandContent.style.maxHeight = expandContent.scrollHeight + 150 + 'px'
         } else {
@@ -15,7 +17,7 @@ const expandBtn = () => {
         }
       })
     });
-  } 
+  }
 }
 
 export { expandBtn }
