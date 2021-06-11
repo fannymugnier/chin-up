e4e2a8da5262c600a24f95ee162370a899a1617c
Fanny Mugnier <fanny.mugnier@outlook.com>
2021-06-11 09:19:49 +0200

    added lord of the rings image suggestions

--- app/javascript/plugins/search_photo_preview.js
+++ app/javascript/plugins/search_photo_preview.js
@@ -9,8 +9,15 @@ const previewImageOnQuery1 = () => {
   const input1 = document.querySelector("#input1");
   if (input1) {
     input1.addEventListener("input", () => {
-      if (input1.value.length > 3)
-        fetchPhoto(input1.value, updateImage1);
+      if (input1.value.length >= 3) {
+        if (input1.value === "Deux Tours" || input1.value === "Deux tours" || input1.value === "deux tours" ) {
+          updateImage1("https://fr.web.img6.acsta.net/medias/nmedia/00/02/54/95/affiche2.jpg")
+        } else if (input1.value === "oui") {
+          updateImage1("https://images.unsplash.com/photo-1580893211123-627e0262be3a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80")
+        } else {
+          fetchPhoto(input2.value, updateImage2);
+        }
+      }
     });
   }
 };
@@ -26,8 +33,15 @@ const previewImageOnQuery2 = () => {
   const input2 = document.querySelector("#input2");
   if (input2) {
     input2.addEventListener("input", () => {
-      if (input2.value.length > 3)
-        fetchPhoto(input2.value, updateImage2);
+      if (input2.value.length >= 3) {
+        if (input2.value === "Retour du Roi" || input2.value === "Retour du roi" || input2.value === "retour du roi") {
+          updateImage2("https://fr.web.img3.acsta.net/medias/nmedia/18/35/14/33/18366630.jpg")
+        } else if (input2.value === "non") {
+          updateImage2("https://www.publicdomainpictures.net/pictures/180000/velka/hand-with-thumb-down.jpg")
+        } else {
+          fetchPhoto(input2.value, updateImage2);
+        }
+      }
     });
   }
 };
