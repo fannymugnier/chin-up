63cc870760937f5b558039060c6bb9f3e935bfd5
Fanny Mugnier <fanny.mugnier@outlook.com>
2021-06-24 19:54:35 +0200

    refacto room_channel.js

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,83 +1,54 @@
 import consumer from "./consumer";
 import { expandBtn } from "../plugins/expand-btn";
 import { initTimer } from "../plugins/timer-topic";
+import { initScrollToBottom } from "../plugins/chat_scroll_bottom";
+import { everyoneHasVoted, displayResult, updatePoll } from "../plugins/chat_poll";
 
-const updatePoll = (data) => {
-  const surveyId = data.survey_id;
-  const firstPropositionCount = data.first_proposition_count;
-  const secondPropositionCount = data.second_proposition_count;
-  const firstPropositionPercent = data.first_proposition_percent;
-  const secondPropositionPercent = data.second_proposition_percent;
-
-  const surveyElement = document.querySelector(`#survey-${surveyId}`);
-  const surveyResultWrap = surveyElement.querySelector(".result-wrap");
-  surveyResultWrap.querySelector(".first-prop-count").innerText =
-    firstPropositionCount;
-  surveyResultWrap.querySelector(".second-prop-count").innerText =
-    secondPropositionCount;
-
-  surveyResultWrap.querySelector(
-    ".results-poll .proposition.first"
-  ).style.width = `${firstPropositionPercent}%`;
-  surveyResultWrap.querySelector(
-    ".results-poll .proposition.second"
-  ).style.width = `${secondPropositionPercent}%`;
-};
+const insertMessage = (container, html) => {
+  container.insertAdjacentHTML('beforeend', html);
+  initScrollToBottom();
+}
 
 const initRoomCable = () => {
   const messagesContainer = document.getElementById("messages");
   if (messagesContainer) {
     const id = messagesContainer.dataset.chatroomId;
-    const a = consumer.subscriptions.create({ channel: "RoomChannel", id: id }, {
+    const newSub = consumer.subscriptions.create({ channel: "RoomChannel", id: id }, {
       received(data) {
-        const surveyContainer = document.querySelector("#survey-container");
-        const surveyResultsContainer = document.querySelector("#survey-results");
-        const topicBanner = document.querySelector("#topic-banner-anchor");
-        const dataParsed = JSON.parse(data)
-        if (dataParsed.event == "new_message") {
-          messagesContainer.insertAdjacentHTML('beforeend', dataParsed.html);
-          messagesContainer.scrollTop = messagesContainer.scrollHeight;
-        } else if (dataParsed.event == "new_topic_announce") {
-          messagesContainer.insertAdjacentHTML('beforeend', dataParsed.html);
-          messagesContainer.scrollTop = messagesContainer.scrollHeight;
-          topicBanner.innerHTML = dataParsed.content;
-          const countDownBis = document.querySelector('#countdown')
-
-          if(countDownBis.hasAttribute('data-end-timer')) {
-
-          } else {
-            countDownBis.setAttribute('data-end-timer', 600)
-            setTimeout(initTimer(), 1000);
-          }
-        } else if (dataParsed.event == "new_survey_announce") {
-          messagesContainer.insertAdjacentHTML('beforeend', dataParsed.html);
-          messagesContainer.scrollTop = messagesContainer.scrollHeight;
-          surveyContainer.insertAdjacentHTML('beforeend', dataParsed.voteHtml);
-        } else if (dataParsed.event == "new_vote") {
-          updatePoll(dataParsed.data)
-          if (dataParsed.data.first_proposition_count + dataParsed.data.second_proposition_count === dataParsed.data.number_of_users) {
-            const winDivElement = document.querySelector(`#winning-prop-survey${dataParsed.data.survey_id}`);
-            const voteElement = document.querySelector('.ongoing-vote');
-            voteElement.classList.add("vote-hide")
-            winDivElement.classList.remove("hidden")
-            if (dataParsed.data.first_proposition_count < dataParsed.data.second_proposition_count) {
-              winDivElement.insertAdjacentHTML('beforeend', `<p>${dataParsed.data.second_proposition} l'emporte ! 🎉</p>`)
-            } else if (dataParsed.data.first_proposition_count > dataParsed.data.second_proposition_count) {
-              winDivElement.insertAdjacentHTML('beforeend', `<p>${dataParsed.data.first_proposition} l'emporte ! 🎉</p>`)
-            } else {
-              winDivElement.insertAdjacentHTML('beforeend', "<p>Ex-æquo ! 🤷‍♂️</p>")
+        const dataParsed = JSON.parse(data);
+        switch (dataParsed.event) {
+          case 'new_message':
+            insertMessage(messagesContainer, dataParsed.html);
+            break;
+          case 'new_topic_announce':
+            insertMessage(messagesContainer, dataParsed.html);
+            document.querySelector("#topic-banner-anchor").innerHTML = dataParsed.content;
+            const countDownBis = document.querySelector('#countdown');
+            if (!countDownBis.hasAttribute('data-end-timer')) {
+              countDownBis.setAttribute('data-end-timer', 600);
+              setTimeout(initTimer(), 1000);
             }
-          }
+            break;
+          case 'new_survey_announce':
+            insertMessage(messagesContainer, dataParsed.html);
+            document.querySelector("#survey-container").insertAdjacentHTML('beforeend', dataParsed.voteHtml);
+            break;
+          case 'new_vote':
+            const vote = dataParsed.data;
+            updatePoll(vote);
+            if (everyoneHasVoted(vote)) {
+              document.querySelector('.ongoing-vote').classList.add("vote-hide");
+              displayResult(vote);
+            }
+            break;
         }
-        },
-      }
-    );
-    const logOutDiv = document.querySelector('.exit-logo')
-
+      },
+    })
+    const logOutDiv = document.querySelector('.exit-logo');
     logOutDiv.addEventListener('click', (e) => {
-      a.unsubscribe();
+      newSub.unsubscribe();
     })
   }
-};
+}
 
 export { initRoomCable };
--- app/javascript/plugins/chat_poll.js
+++ app/javascript/plugins/chat_poll.js
@@ -1,7 +1,31 @@
-// first proposition
-const firstProposition = document.querySelector(".proposition.first");
-// second proposition
-const secondProposition = document.querySelector(".proposition.second");
-// poll div
-const pollDiv = document.querySelector(".proposition");
-// TODO ...
+const everyoneHasVoted = (data) => {
+  return data.first_proposition_count + data.second_proposition_count === data.number_of_users;
+}
+
+const displayResult = (surveyData) => {
+  const textResultElement = document.querySelector(`#winning-prop-survey${vote.survey_id}`);
+  if (surveyData.first_proposition_count < surveyData.second_proposition_count) {
+    textResultElement.insertAdjacentHTML('beforeend', `<p>${surveyData.second_proposition} l'emporte ! 🎉</p>`);
+  } else if (surveyData.first_proposition_count > surveyData.second_proposition_count) {
+    textResultElement.insertAdjacentHTML('beforeend', `<p>${surveyData.first_proposition} l'emporte ! 🎉</p>`);
+  } else {
+    textResultElement.insertAdjacentHTML('beforeend', "<p>Ex-æquo ! 🤷‍♂️</p>");
+  }
+}
+
+const updatePoll = (data) => {
+  const surveyId = data.survey_id;
+  const firstPropCount = data.first_proposition_count;
+  const secondPropCount = data.second_proposition_count;
+  const firstPropPercent = data.first_proposition_percent;
+  const secondPropPercent = data.second_proposition_percent;
+  const surveyElement = document.querySelector(`#survey-${surveyId}`);
+  const surveyResultWrap = surveyElement.querySelector(".result-wrap");
+
+  surveyResultWrap.querySelector(".first-prop-count").innerText =firstPropCount;
+  surveyResultWrap.querySelector(".second-prop-count").innerText = secondPropCount;
+  surveyResultWrap.querySelector(".results-poll .proposition.first").style.width = `${firstPropPercent}%`;
+  surveyResultWrap.querySelector(".results-poll .proposition.second").style.width = `${secondPropPercent}%`;
+}
+
+export { everyoneHasVoted, displayResult, updatePoll };
